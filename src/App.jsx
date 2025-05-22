7506639410928b366b1e32d15445b28ca2ee2e42
Nataly <ntuong1117@gmail.com>
2025-05-21 22:02:47 -0700

    more kitties!

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,56 @@ const App = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  const cats = [
+    {
+      imageSrc: "/zazu.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      imageSrc: "/tole-tole.jpg",
+      text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      imageSrc: "/kiji.jpg",
+      text: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    },
+    {
+      imageSrc: "/dou-dou.jpg",
+      text: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+    },
+    {
+      imageSrc: "/mr-fresh.jpg",
+      text: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
+    },
+  ];
+
+  const CatCard = ({ imageSrc, text }) => {
+    return (
+      <div
+        className={`p-2 rounded-2xl smooth-transition rounded-2xl bg-dark-container w-full h-50 hover:scale-102 hover:shadow-sm hover:shadow-black/40 ${
+          isDarkMode ? "bg-dark-container" : "bg-light-primary"
+        }`}
+      >
+        <div className="  rounded-2xl h-full text-white grid grid-cols-[1fr_2fr] gap-4">
+          <div className="overflow-hidden">
+            <img
+              src={imageSrc}
+              alt="cat"
+              className="w-full h-full rounded-2xl max-w-45 object-fill"
+            ></img>
+          </div>
+          <div
+            className={`h-40 overflow-y-auto pr-2 py-4 smooth-transition ${
+              isDarkMode ? "text-dark-text" : "text-light-text"
+            }`}
+          >
+            {text}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     // Background with transition
     <div
@@ -55,25 +105,14 @@ const App = () => {
 
         {/* Grid Stuff */}
         <div
-          className={`p-4 shadow-xl shadow-black/35 smooth-transition rounded-3xl flex-1 p-4 ${
+          className={`overflow-y-auto h-200 px-5 pr-5 p-4 shadow-xl shadow-black/35 smooth-transition rounded-3xl p-4 ${
             isDarkMode ? "bg-dark-primary" : "bg-light-container"
           }`}
         >
-          <div
-            className={`p-2 rounded-2xl smooth-transition rounded-2xl bg-dark-container w-full h-50 hover:scale-102 hover:shadow-sm hover:shadow-black/40 ${
-              isDarkMode ? "bg-dark-container" : "bg-light-primary"
-            }`}
-          >
-            <div className="  rounded-2xl h-full text-white grid grid-cols-[1fr_2fr] gap-4">
-              <div className="overflow-hidden rounded-2xl">
-                <img
-                  src="/zazu.jpg"
-                  alt="zazu"
-                  className="w-full h-full object-fill"
-                ></img>
-              </div>
-              <div></div>
-            </div>
+          <div className="grid grid-cols-1 gap-4 flex-1">
+            {cats.map((cat, index) => (
+              <CatCard key={index} imageSrc={cat.imageSrc} text={cat.text} />
+            ))}
           </div>
         </div>
       </div>
